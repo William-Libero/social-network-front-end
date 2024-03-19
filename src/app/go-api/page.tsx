1b4a8174572f983588006ad213bd6d23b27bad8a
william <williamlibero@profkenny.com.br>
2024-03-18 21:23:47 -0300

    Adicionado rotas que encaminham para o projeto no github.

---
+++ src/app/go-api/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { selectCurrentPage } from "@/lib/features/currentPage/currentPageSlice";
+import { useAppSelector } from "@/lib/hooks";
+
+export default function GoApi() {
+  window.location.replace(
+    "https://github.com/William-Libero/social-networking-posts-and-rabbitmq"
+  );
+}
