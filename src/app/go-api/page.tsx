6b619d9f9710c4ab7cb84e1ade19f6c01fa59a05
william <williamlibero@profkenny.com.br>
2024-03-18 21:31:09 -0300

    Adicionado rotas que encaminham para o projeto no github.

--- src/app/go-api/page.tsx
+++ src/app/go-api/page.tsx
@@ -3,9 +3,11 @@ import { selectCurrentPage } from "@/lib/features/currentPage/currentPageSlice";
 import { useAppSelector } from "@/lib/hooks";
 
 export default function GoApi() {
-  window.location.replace(
-    "https://github.com/William-Libero/social-networking-posts-and-rabbitmq"
-  );
+  if (typeof window !== "undefined") {
+    window.location.replace(
+      "https://github.com/William-Libero/social-networking-posts-and-rabbitmq"
+    );
+  }
 
   return <div></div>;
 }
--- src/app/nest-api/page.tsx
+++ src/app/nest-api/page.tsx
@@ -3,9 +3,11 @@ import { selectCurrentPage } from "@/lib/features/currentPage/currentPageSlice";
 import { useAppSelector } from "@/lib/hooks";
 
 export default function NestApi() {
-  window.location.replace(
-    "https://github.com/William-Libero/social-networking-posts-service"
-  );
+  if (typeof window !== "undefined") {
+    window.location.replace(
+      "https://github.com/William-Libero/social-networking-posts-service"
+    );
+  }
 
   return <div></div>;
 }
